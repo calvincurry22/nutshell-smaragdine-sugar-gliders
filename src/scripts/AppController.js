4cd13e1f65e9eece886970099dc436657c2b70a7
durrjp <durrjp1@gmail.com>
2020-04-15 14:16:02 -0500

    switching branches

--- src/scripts/AppController.js
+++ src/scripts/AppController.js
@@ -1,4 +1,5 @@
-// Jon Durr - app controller component that will control the current page state of the app
+// Jon Durr - app controller component that will control the current page state of the app,
+// 
 
 import { Dashboard } from "./Dashboard.js"
 
