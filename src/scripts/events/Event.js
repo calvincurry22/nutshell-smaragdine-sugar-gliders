51d595ca94b2c98dfb0df61b3a652f22bf05a5d1
Keartroth <mecarrolljr@gmail.com>
2020-04-16 15:02:18 -0500

    added styling to the events cards.

--- src/scripts/events/Event.js
+++ src/scripts/events/Event.js
@@ -3,13 +3,15 @@
 export const EventHTML = (event, isAFriendClass, userWhoWroteEvent, currentUserId) => {
     return `
     <div class="event ${isAFriendClass === true ? 'italicized' : ''}">
-        <div class="event__title">${event.event}</div>
+        <div class="eventTitleDeleteButtonWrapper">
+            <div class="event__title">${event.event}</div>
+            ${event.user === currentUserId ? `<button id="deleteEventBtn--${event.id}">x</button>`:""}
+        </div>
         <div class="textImageWrapper">
             <div class="event__text">
                 <div class="event__detail">${event.date}</div>
                 <div class="event__detail">${event.location}</div>
                 <div class="event__detail">posted by: ${userWhoWroteEvent.username}</div>
-                ${event.user === currentUserId ? `<button id="deleteEventBtn--${event.id}">x</button>`:""}
             </div>
             <div class="event_imageContainer">
                 <img class="event__image" src="../../styles/images/calendar.svg">
