f84132357c604cc27f2d064f683c12cfcbfa59cc
durrjp <durrjp1@gmail.com>
2020-04-16 14:22:52 -0500

    chat box styling

--- src/scripts/messages/Message.js
+++ src/scripts/messages/Message.js
@@ -2,7 +2,11 @@
 
 export const Message = (message, messagesUser, currentUserId) => {
     return `
-    <div class="messages__message">${messagesUser.username}: ${message.messageText}</div>
-    ${message.userId === currentUserId ? `<button id="deleteMessageBtn--${message.id}">x</button>`:""}
+    <div class="messages__message  ">
+        <div class="messages__message__container ${message.userId === currentUserId ? 'left':'right'}">
+            <div>${messagesUser.username}: ${message.messageText}</div>
+            ${message.userId === currentUserId ? `<button id="deleteMessageBtn--${message.id}">x</button>`:""}
+        </div>
+    </div>
     `
 }
