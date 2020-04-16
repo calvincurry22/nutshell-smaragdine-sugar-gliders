8792482ea0b8d2064de658571e9cfb239f3a05f4
Keartroth <mecarrolljr@gmail.com>
2020-04-16 14:16:27 -0500

    commiting to switch branches for a review.

--- src/scripts/events/Event.js
+++ src/scripts/events/Event.js
@@ -4,10 +4,17 @@ export const EventHTML = (event, isAFriendClass, userWhoWroteEvent, currentUserI
     return `
     <div class="event ${isAFriendClass === true ? 'italicized' : ''}">
         <div class="event__title">${event.event}</div>
-        <div class="event__detail">${event.date}</div>
-        <div class="event__detail">${event.location}</div>
-        <div class="event__detail">posted by: ${userWhoWroteEvent.username}</div>
-        ${event.user === currentUserId ? `<button id="deleteEventBtn--${event.id}">x</button>`:""}
+        <div class="textImageWrapper">
+            <div class="event__text">
+                <div class="event__detail">${event.date}</div>
+                <div class="event__detail">${event.location}</div>
+                <div class="event__detail">posted by: ${userWhoWroteEvent.username}</div>
+                ${event.user === currentUserId ? `<button id="deleteEventBtn--${event.id}">x</button>`:""}
+            </div>
+            <div class="event_imageContainer">
+                <img class="event__image" src="../../styles/images/calendar.svg">
+            </div>
+        </div>
     </div>
     `
 }
